0da0ecabb58a8a03360bde2c6378013710f676a5
andres <tech49060@gmail.com>
2026-03-11 08:39:51 -0500

    Database configuration file: local conn

---
+++ config/example.php
@@ -0,0 +1,22 @@
+<?php
+/*
+    echo "Hello world. ";
+
+    $num1 = 20;
+    $num2 = 5;
+    $add = $num1 + $num2;
+
+    echo "<br>";
+    echo "la suma de ". $num1 ." + ". $num2 . " es: ". $add;
+
+
+    if ($num1 > $num2){
+        echo "<br>";
+        echo " ganaste";
+    }else{
+        echo "<br>";
+        echo " perdiste puta";
+    }
+*/
+
+?>

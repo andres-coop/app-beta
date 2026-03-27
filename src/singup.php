87c8cce3d091bcedcf5aaf1947623c9a5890b626
andres <tech49060@gmail.com>
2026-03-27 12:06:31 -0500

    Feature1-RegisterUser-Backend

--- src/singup.php
+++ src/singup.php
@@ -16,9 +16,27 @@
     //query to insert into SQL
     $sql = "INSERT INTO users (firstname, lastname, email,mobile_phone, password ) 
     VALUES ('$f_name', '$l_name', '$email', '$m_phone', '$enc_pass')";
+
+    //email RAMA 1 
+    $check_email = "SELECT email FROM users WHERE email = '$email'";
+    $res_email = pg_query($local_conn, $check_email);
+
+    if (pg_num_rows($res_email) > 0) {
+        echo "Error: El correo electrónico '$email' ya está registrado. Por favor, use uno diferente.\n";
+        exit();
+    }
+    
+    $res_local = pg_query($local_conn, $sql);
+    
+    
+
+
+
+
+
     
     //execute query
-    pg_query($sql);
+    //pg_query($sql);
 
 
 
